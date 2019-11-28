afbfb6da7a325c6a0140ee6938a6cb9ac027ec96
KadoTomohiro <>
2019-11-29 00:02:52 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kfng-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngDx';
+}
