946dc680ecab8120556dffe343d4a7f86ebdae90
KadoTomohiro <>
2019-12-03 18:16:37 +0900

    styling bug fix

--- projects/kfng/src/lib/fix-when-scrolling.directive.ts
+++ projects/kfng/src/lib/fix-when-scrolling.directive.ts
@@ -1,36 +1,53 @@
 import {Directive, ElementRef, Inject, OnInit, Renderer2} from '@angular/core';
 import {WINDOW} from './window-token';
+
 @Directive({
   selector: '[kfngFixWhenScrolling]',
 })
 export class FixWhenScrollingDirective implements OnInit {
 
-  constructor(private el: ElementRef<HTMLElement>, private renderer: Renderer2, @Inject(WINDOW) private windowRef: any) {
+  constructor(private el: ElementRef<HTMLElement>, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
     const ne = this.el.nativeElement;
     const offsetTop = ne.offsetTop;
+
+    this.renderer.listen('window', 'scroll', (event) => {
+        const scrollY = event.currentTarget.scrollY;
+        if (scrollY > offsetTop) {
+          this.setStyles(ne);
+        } else {
+          this.removeStyles(ne);
+        }
+      }
+    );
+    this.renderer.listen('window', 'resize', (event) => {
+      const scrollY = event.currentTarget.scrollY;
+      if (scrollY > offsetTop) {
+        this.removeStyles(ne);
+        this.setStyles(ne);
+      }
+    });
+  }
+
+  private removeStyles(ne: HTMLElement) {
+    const styles = ['position', 'top', 'left', 'width', 'z-index'];
+    for (const style of styles) {
+      this.renderer.removeStyle(ne, style);
+    }
+  }
+
+  private setStyles(ne: HTMLElement) {
     const styles = {
       position: 'fixed',
       top: '0',
       left: ne.offsetLeft.toString(),
-      width: this.windowRef.getComputedStyle(ne).width,
+      width: window.getComputedStyle(ne).width,
       'z-index': '2',
     };
-
-    this.renderer.listen('window', 'scroll',
-      (event) => {
-        const scrollY = event.currentTarget.scrollY;
-        if (scrollY > offsetTop) {
-          for (const [style, styleValue] of Object.entries(styles)) {
-            this.renderer.setStyle(ne, style, styleValue);
-          }
-        } else {
-          for (const style of Object.keys(styles)) {
-            this.renderer.removeStyle(ne, style);
-          }
-        }
-      });
+    for (const [style, styleValue] of Object.entries(styles)) {
+      this.renderer.setStyle(ne, style, styleValue);
+    }
   }
 }
